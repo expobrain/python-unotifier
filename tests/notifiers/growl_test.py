7e95885c376e0ca570c36bf432ebcb6be0226d9c
Daniele Esposti <expo@expobrain.net>
2016-01-24 10:41:59 +0000

    Implemented Growl notifier

--- tests/notifiers/growl_test.py
+++ tests/notifiers/growl_test.py
@@ -2,6 +2,20 @@ from __future__ import unicode_literals
 
 import unittest
 
+from unotifier.notifiers import GrowlNotifier
 
-class GrowlNotifierTests(unittest.TestCase):
-    pass
+from . import NotifierTestsMixin
+
+
+class GrowlNotifierTests(NotifierTestsMixin, unittest.TestCase):
+
+    notifier_klass = GrowlNotifier
+
+    def setUp(self):
+        super(GrowlNotifierTests, self).setUp()
+
+    def tearDown(self):
+        super(GrowlNotifierTests, self).tearDown()
+
+    def test_notify(self):
+        pass
--- unotifier/notifiers/growl.py
+++ unotifier/notifiers/growl.py
@@ -1,5 +1,43 @@
 from __future__ import unicode_literals
 
+import gntp.notifier
 
-class GrowlNotifier(object):
-    pass
+from .abstract import AbstractNotifier
+
+
+class GrowlNotifier(AbstractNotifier):
+
+    DEFAULT_APP_NAME = 'Python'
+    DEFAULT_HOSTNAME = 'localhost'
+
+    def __init__(self, *args, **kwds):
+        super(GrowlNotifier, self).__init__(*args, **kwds)
+
+        options = kwds.get('options', {})
+
+        self.growl = gntp.notifier.GrowlNotifier(
+            applicationName=options.get('name', self.DEFAULT_APP_NAME),
+            hostname=options.get('host', self.DEFAULT_HOSTNAME)
+        )
+        self.growl.register()
+
+    def get_cmd_options(self, options):
+        options = self._map_app_icon(options)
+        options = self._map_icon_shorthand(options)
+
+        if options.get('text'):
+            options['message'] = options.pop('text')
+
+        return options
+
+    def notify(self, message, title=None, icon=None, wait=None):
+        # Prepare options
+        options = self.options
+        options['message'] = message
+        options['title'] = title
+        options['icon'] = icon
+
+        # Get command options
+        cmd_options = self.get_cmd_options(options)
+
+        self.growl.notify(**cmd_options)
